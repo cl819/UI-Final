5ed4aee38edd360d62e1b251a32e59af5fe9b761
Courtney Lam <cl819@scarletmail.rutgers.edu>
2016-05-05 21:44:25 -0400

    bugged

---
+++ Assets/Menu.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+
+	private Animator _animator;
+	private CanvasGroup _canvasGroup;
+
+	public bool IsOpen{
+		get{ return _animator.GetBool ("IsOpen"); }
+		set{ _animator.SetBool ("IsOpen", value);}
+	}
+
+	public void Awake(){
+		_animator = GetComponent<Animator> ();
+		_canvasGroup = GetComponent<CanvasGroup>(); 
+
+		var rect = GetComponent<RectTransform> ();
+		rect.offsetMax = rect.offsetMin = new Vector2 (0, 0);
+	}
+
+	public void Update(){
+		if (!_animator.GetCurrentAnimatorStateInfo (0).IsName ("Open")) {
+			_canvasGroup.blocksRaycasts = _canvasGroup.interactable = false; // when disabled you won't have overlapping menus 
+		} else {
+			_canvasGroup.blocksRaycasts = _canvasGroup.interactable = true;
+		}
+	}//end update
+}//end class
